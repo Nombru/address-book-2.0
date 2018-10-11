3d4091284175932623658b04b39c0860aa02e9b1
jentry j and richard laws <richard@>
2018-10-10 20:08:40 -0700

    add functionality to Adress constructor and allow inputs to be added to the Contact constructor inputs, also will add ability to append contact form to restore form to original state

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,93 @@
 //business logic
+
+// Name Constructor
 function Contact(first, last) {
   this.firstName = first;
   this.lastName = last;
   this.addresses = [];
-
 }
 
+//Address Contructor
+function Address(street, city, state) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
+}
+
+// New Prototype Method for Contact
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
 
+// New Prototype Method for Addreses
+Address.prototype.fullAddress = function() {
+  return this.street + ", " + this.city + " " + this.state;
+}
+
 // user interface logic
 $(document).ready(function() {
+  $("#add-address").click(function() {
+    $("#new-addresses").append('<div class="new-address">' +
+                                 '<div class= "next-address">'+
+                                  '<div class="form-group">' +
+                                     '<label for="new-street">Street</label>' +
+                                     '<input type="text" class="form-control new-street">' +
+                                     // ALT: possible alternative way to append the form
+                                     // '<input type="text" class="form-control new-street2">' +
+                                   '</div>' +
+                                   '<div class="form-group">' +
+                                     '<label for="new-city">City</label>' +
+                                     '<input type="text" class="form-control new-city">' +
+                                   '</div>' +
+                                   '<div class="form-group">' +
+                                     '<label for="new-state">State</label>' +
+                                     '<input type="text" class="form-control new-state">' +
+                                   '</div>' +
+                                  '</div>' +
+                                '</div>');
+  });
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
+
+    $(".next-address").remove()
 
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
 
+// Creating new contact
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
+// Creating address from input information
+    $(".new-address").each(function() {
+      var inputtedStreet = $(this).find("input.new-street").val();
+      // ALT: possible alternative way to append form
+      // var inputtedStreet = $("input.new-street2").val();
+      var inputtedCity = $(this).find("input.new-city").val();
+      var inputtedState = $(this).find("input.new-state").val();
+      var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState);
+      newContact.addresses.push(newAddress);
+    });
+
 // Appending UL with List Items of captured address content
     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+
+
 // Showing the stored values
     $(".contact").last().click(function() {
       $("#show-contact").show();
       $("#show-contact h2").text(newContact.firstName);
       $(".first-name").text(newContact.firstName);
       $(".last-name").text(newContact.lastName);
+// Show the addresses that were entered with contact.
+      $("ul#addresses").text("");
+      newContact.addresses.forEach(function(address) {
+        $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
+      });
     });
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
+    $("input.new-street").val("");
+    $("input.new-city").val("");
+    $("input.new-state").val("");
   });
 });
